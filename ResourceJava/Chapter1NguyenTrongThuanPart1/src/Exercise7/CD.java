a1bd00f3ff52c530c95771e1f4d2e7be69fb7f07
Nguyen Trong Thuan <ntthuanhcmus@gmail.com>
2016-08-20 15:41:31 +0700

    completed ex 7

---
+++ ResourceJava/Chapter1NguyenTrongThuanPart1/src/Exercise7/CD.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercise7;
+
+/**
+ *
+ * @author Nguyen Trong Thuan
+ * @date: 20/08/2016
+ * @version 1
+ * ---------------------------
+ * create class CD
+ */
+public class CD {
+    private String id;
+    private String name;
+    private String singer;
+    private int numOfSong;
+    private double price;
+
+    public CD() {
+    }
+
+    public CD(String id, String name, String singer, int numOfSong, double price) {
+        this.id = id;
+        this.name = name;
+        this.singer = singer;
+        this.numOfSong = numOfSong;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public int getNumOfSong() {
+        return numOfSong;
+    }
+
+    public void setNumOfSong(int numOfSong) {
+        this.numOfSong = numOfSong;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    
+    /**
+     * function show information of CD
+     * Output: information of CD
+     */
+    
+    public void showCD(){
+        System.out.println("Information of CD:");
+        System.out.println("- ID: " + this.getId());
+        System.out.println("- CD name:" + this.getName());
+        System.out.println("- Singer: " + this.getSinger());
+        System.out.println("- Number of song: " + this.getNumOfSong());
+        System.out.println(String.format("- Price: %.3f", this.getPrice()));
+    }
+}
---
+++ ResourceJava/Chapter1NguyenTrongThuanPart1/src/Exercise7/Main.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercise7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Nguyen Trong Thuan
+ * @date: 08/06/2016
+ * @version 1
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String option = "y";
+            ArrayList<CD> arrayCD = new ArrayList<CD>();
+            while (option.equals("y")) {
+                System.out.println("-----------------------------------------");
+                System.out.println("Input information of CD: ");
+                System.out.print("Input ID: ");
+                String id = input.readLine();
+                System.out.print("Input name of CD: ");
+                String name = input.readLine();
+                System.out.print("Input singer: ");
+                String singer = input.readLine();
+                System.out.print("Input number of song: ");
+                int numOfSong = Integer.parseInt(input.readLine());
+                if (numOfSong <= 0) {
+                    throw new ArithmeticException("Number of song is not valid!");
+                }
+                System.out.print("Input price: ");
+                double price = Double.parseDouble(input.readLine());
+                if (price <= 0) {
+                    throw new ArithmeticException("Price is not valid!");
+                }
+                arrayCD.add(new CD(id, name, singer, numOfSong, price));
+                System.out.println("------------------------------------------");
+                System.out.println("Do you want to add new CD ? (y/n)");
+                option = input.readLine().toLowerCase();
+            }
+            System.out.println("------------------------------------------");
+            double totalPrice = 0;
+            for (CD cd : arrayCD) {
+                cd.showCD();
+                totalPrice += cd.getPrice();
+            }
+            System.out.println("------------------------------------------");
+            System.out.println(String.format("Total price: %.3f", totalPrice));
+        } catch (NumberFormatException | ArithmeticException ex) {
+            System.out.println("ERROR: " + ex.getMessage());
+        }
+    }
+
+}
