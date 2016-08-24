cafb46be0caac6c367fc5ce9209034a4fdd6f067
Nguyen Trong Thuan <ntthuanhcmus@gmail.com>
2016-08-24 10:24:33 +0700

    Update Trooper.java

--- ResourceJava/Chapter1NguyenTrongThuanPart1/src/Exercise8/Trooper.java
+++ ResourceJava/Chapter1NguyenTrongThuanPart1/src/Exercise8/Trooper.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Exercise8;
 
 /**
@@ -18,7 +13,10 @@ public class Trooper extends Soldier {
     public Trooper(String name, int power, String weapon) {
         super(name, power, weapon);
     }
-
+    
+    /**
+     * function fight power of trooper
+     */ 
     @Override
     public void fight() {
         this.setPower(this.power - 3);
