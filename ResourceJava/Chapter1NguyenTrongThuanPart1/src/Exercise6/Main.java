96f321dfce4b7680118fd934b4b83e29ea0f2f03
Nguyen Trong Thuan <ntthuanhcmus@gmail.com>
2016-08-24 10:13:18 +0700

    Update Main.java

--- ResourceJava/Chapter1NguyenTrongThuanPart1/src/Exercise6/Main.java
+++ ResourceJava/Chapter1NguyenTrongThuanPart1/src/Exercise6/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Exercise6;
 
 import java.io.BufferedReader;
@@ -48,5 +43,4 @@ public class Main {
             System.out.println("ERROR: " + ex.getMessage());
         }
     }
-    
 }
