49e5307193f5a096c3df5d8767d550c5a52c3f2f
Nguyen Trong Thuan <ntthuanhcmus@gmail.com>
2016-08-24 10:41:50 +0700

    Update Student.java

--- ResourceJava/Chapter1NguyenTrongThuanPart1/src/Exercise10/Student.java
+++ ResourceJava/Chapter1NguyenTrongThuanPart1/src/Exercise10/Student.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Exercise10;
 
 /**
@@ -71,5 +67,4 @@ public class Student extends Human{
         System.out.println("Class: " + this.studentOfClass);
         System.out.println("Average score: " + this.calculateAverageScore());
     }
-    
 }
