5b06bfc62faca383a0686d4dea5b3bcf0e574071
Nguyen Trong Thuan <ntthuanhcmus@gmail.com>
2016-08-24 10:00:49 +0700

    Update Point.java

--- ResourceJava/Chapter1NguyenTrongThuanPart1/src/Exercise3/Point.java
+++ ResourceJava/Chapter1NguyenTrongThuanPart1/src/Exercise3/Point.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Exercise3;
 
 /**
@@ -10,7 +5,7 @@ package Exercise3;
  * @author Nguyen Trong Thuan
  * @date: 19/08/2016
  * @version 1
- * class: point
+ * class: point to manager point
  */
 public class Point {
 
