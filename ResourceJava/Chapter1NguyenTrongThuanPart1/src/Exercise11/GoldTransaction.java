6d77fa827c5c49675ecfa2ea24a52194da661eb2
Nguyen Trong Thuan <ntthuanhcmus@gmail.com>
2016-08-24 10:49:10 +0700

    Update GoldTransaction.java

--- ResourceJava/Chapter1NguyenTrongThuanPart1/src/Exercise11/GoldTransaction.java
+++ ResourceJava/Chapter1NguyenTrongThuanPart1/src/Exercise11/GoldTransaction.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Excercise11;
 
 /**
@@ -50,5 +45,4 @@ public class GoldTransaction extends Transaction {
         }
         return result;
     }
-    
 }
