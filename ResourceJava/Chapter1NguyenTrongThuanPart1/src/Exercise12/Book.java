60355e43c30cce825e6ff29423423a10a68dd366
Nguyen Trong Thuan <ntthuanhcmus@gmail.com>
2016-08-24 11:02:32 +0700

    Update Book.java

--- ResourceJava/Chapter1NguyenTrongThuanPart1/src/Exercise12/Book.java
+++ ResourceJava/Chapter1NguyenTrongThuanPart1/src/Exercise12/Book.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Exercise12;
 
 /**
@@ -101,5 +96,4 @@ public class Book {
         System.out.println("Amount: " + this.amount);
         System.out.println("Pulisher: " + this.publisher);
     }
-    
 }
