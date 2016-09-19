71470bd0b16f7b395ae2b29a69c54c67cd9835d5
Nguyen Trong Thuan <ntthuanhcmus@gmail.com>
2016-09-19 22:39:27 +0700

    submit

--- BigExcercise2NguyenTrongThuan/src/Part2_Ex1/CDController.java
+++ BigExcercise2NguyenTrongThuan/src/Part2_Ex1/CDController.java
@@ -52,20 +52,17 @@ public class CDController {
 	// add new CD with parameter CD
 	public void addCD(CD obj) throws ClassNotFoundException, SQLException {
 		try (Connection conn = db.connect()) {
-			String sql = "INSERT INTO cd VALUES(NULL, '" + obj.getName()
-					+ "', '" + obj.getSinger() + "', " + obj.getNumOfSong()
-					+ ", " + obj.getPrice() + ")";
-			PreparedStatement statement = (PreparedStatement) conn
-					.prepareStatement(sql);
+			String sql = "INSERT INTO cd VALUES(NULL, '" + obj.getName() + "', '" + obj.getSinger() + "', "
+					+ obj.getNumOfSong() + ", " + obj.getPrice() + ")";
+			PreparedStatement statement = (PreparedStatement) conn.prepareStatement(sql);
 			statement.execute();
 		}
 	}
 
-	// add new product to  MAIN METHOD
+	// add new product to MAIN METHOD
 	public void runAddCD() throws IOException, ClassNotFoundException, SQLException {
 
-		BufferedReader input = new BufferedReader(new InputStreamReader(
-				System.in));
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("+++++++ Add new CD +++++++");
 		System.out.print("Name: ");
 		String name = input.readLine();
@@ -76,8 +73,10 @@ public class CDController {
 		System.out.print("Price:");
 		double price = Double.parseDouble(input.readLine());
 
-		if(numbersong <= 0) throw new ArithmeticException("Number song is not valid!");
-		if(price <= 0) throw new ArithmeticException("Price is not valid!");
+		if (numbersong <= 0)
+			throw new ArithmeticException("Number song is not valid!");
+		if (price <= 0)
+			throw new ArithmeticException("Price is not valid!");
 
 		try {
 			CD p = new CD(name, singer, numbersong, price);
@@ -90,8 +89,7 @@ public class CDController {
 	}
 
 	// search CD with parameter name of CD -> return list CD
-	public static List<CD> searchCD(String name) throws ClassNotFoundException,
-			SQLException {
+	public static List<CD> searchCD(String name) throws ClassNotFoundException, SQLException {
 		List<CD> rs;
 		try (Connection conn = db.connect()) {
 			rs = new ArrayList<>();
@@ -112,10 +110,8 @@ public class CDController {
 	}
 
 	// excecute search product
-	public void runSearchCD() throws IOException, SQLException,
-			ClassNotFoundException {
-		BufferedReader input = new BufferedReader(new InputStreamReader(
-				System.in));
+	public void runSearchCD() throws IOException, SQLException, ClassNotFoundException {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("+++++++ Search +++++++");
 		System.out.print("Input name CD to search: ");
 		String name = input.readLine();
@@ -135,93 +131,93 @@ public class CDController {
 	}
 
 	// update CD with parameter is CD (UPDATE)
-	public void updateCD(CD cd) throws ClassNotFoundException,
-			SQLException {
+	public void updateCD(CD cd) throws ClassNotFoundException, SQLException {
 		try (Connection conn = db.connect()) {
-			String sql = "UPDATE cd set numbersongs = " + cd.getNumOfSong()
-					+ ", price = " + cd.getPrice() + " where id = "
-					+ cd.getId() + "";
-			PreparedStatement statement = (PreparedStatement) conn
-					.prepareStatement(sql);
+			String sql = "UPDATE cd set numbersongs = " + cd.getNumOfSong() + ", price = " + cd.getPrice()
+					+ " where id = " + cd.getId() + "";
+			PreparedStatement statement = (PreparedStatement) conn.prepareStatement(sql);
 			statement.execute();
 		}
 	}
-	
-	
+
 	// get CD from ID of CD -> return CD / null
-	public CD getCD(List<CD> lst, int id){
-		for(CD cd : lst){
-			if(cd.getId() == id){
+	public CD getCD(List<CD> lst, int id) {
+		for (CD cd : lst) {
+			if (cd.getId() == id) {
 				return cd;
 			}
 		}
 		return null;
 	}
-	
+
 	// run update CD MAIN METHOD
-	public void runUpdateCD() throws NumberFormatException, IOException, ClassNotFoundException, SQLException{
-		BufferedReader input = new BufferedReader(new InputStreamReader(
-				System.in));
+	public void runUpdateCD() throws NumberFormatException, IOException, ClassNotFoundException, SQLException {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("+++++++++ List CD ++++++++");
 		List<CD> lst = this.getAllCD();
-		for(CD cd : lst){
+		for (CD cd : lst) {
 			System.out.println(cd.toString());
 		}
 		System.out.println("+++++++++++++++++++++++++++");
 		System.out.print("Choice id of CD to update: ");
 		int id = Integer.parseInt(input.readLine());
-		System.out.println("+++++++ Input information update +++++++");
-		System.out.print("Input number songs: ");
-		int numbersong = Integer.parseInt(input.readLine());
-		System.out.print("Input price: ");
-		double price = Double.parseDouble(input.readLine());
 		
-		if(numbersong <= 0) throw new ArithmeticException("Number song is not valid!");
-		if(price <= 0) throw new ArithmeticException("Price is not valid!");
-		CD cd = this.getCD(lst, id);
-		cd.setNumOfSong(numbersong);
-		cd.setPrice(price);
-		this.updateCD(cd);
-		System.out.println("Update completed!");
+		CD cd = getCD(lst, id);
+		if (cd == null) {
+			System.out.println("ID is not valid!");
+		} else {
+			System.out.println("+++++++ Input information update +++++++");
+			System.out.print("Input number songs: ");
+			int numbersong = Integer.parseInt(input.readLine());
+			System.out.print("Input price: ");
+			double price = Double.parseDouble(input.readLine());
+
+			if (numbersong <= 0)
+				throw new ArithmeticException("Number song is not valid!");
+			if (price <= 0)
+				throw new ArithmeticException("Price is not valid!");
+			cd.setNumOfSong(numbersong);
+			cd.setPrice(price);
+			this.updateCD(cd);
+			System.out.println("Update completed!");
+		}
 	}
 
 	// delete CD with parameter is CD (DELETE)
-	public void deleteCD(CD cd) throws ClassNotFoundException,
-			SQLException {
+	public void deleteCD(CD cd) throws ClassNotFoundException, SQLException {
 		try (Connection conn = db.connect()) {
 			String sql = "DELETE FROM cd WHERE id = " + cd.getId() + "";
-			PreparedStatement statement = (PreparedStatement) conn
-					.prepareStatement(sql);
+			PreparedStatement statement = (PreparedStatement) conn.prepareStatement(sql);
 			statement.execute();
 		}
 	}
-	
+
 	// run delete CD MAIN METHOD
-	public void runDeleteCD() throws ClassNotFoundException, SQLException, NumberFormatException, IOException{
-		BufferedReader input = new BufferedReader(new InputStreamReader(
-				System.in));
+	public void runDeleteCD() throws ClassNotFoundException, SQLException, NumberFormatException, IOException {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("+++++++++ List CD ++++++++");
 		List<CD> lst = this.getAllCD();
-		for(CD cd : lst){
+		for (CD cd : lst) {
 			System.out.println(cd.toString());
 		}
 		System.out.println("+++++++++++++++++++++++++++");
 		System.out.print("Choice id of CD to delete: ");
 		int id = Integer.parseInt(input.readLine());
-		if(id <= 0) throw new ArithmeticException("ID is not valid!");
+		if (id <= 0)
+			throw new ArithmeticException("ID is not valid!");
 		CD cd = this.getCD(lst, id);
 		System.out.println("Information of CD: ");
 		System.out.println(cd.toString());
 		System.out.println("Do you want delete this CD: (1-yes/ 2-no) ?");
 		int choice = Integer.parseInt(input.readLine());
-		if(choice == 1){
+		if (choice == 1) {
 			this.deleteCD(cd);
 			System.out.println("Delete CD completed!");
-		}else if(choice == 2){
+		} else if (choice == 2) {
 			System.out.println("You have cancel delete CD!");
-		}else{
+		} else {
 			throw new ArithmeticException("Choice is not valid!");
 		}
-		
+
 	}
 }
